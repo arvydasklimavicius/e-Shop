a19e02fd032bca5f118e4863d8298dfee20b2aa6
Arvydas Klimavicius <arvydas.klimavicius@icloud.com>
2020-02-07 10:19:37 +0200

    First Commit

---
+++ Admin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Admin
+//
+//  Created by Arvydas Klimavicius on 2020-02-07.
+//  Copyright © 2020 Arvydas Klimavicius. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
